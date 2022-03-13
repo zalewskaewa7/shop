4e6adeb0e9065874b854c0a57423a6d54238175a
zalewskaewa7 <zalewskaewa7@wp.pl>
2022-03-14 00:33:30 +0100

    zainsalowany projekt

---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,16 @@
+<haeder class="haeder"> 
+    <div id="menu">
+<nav>
+    
+       <a  href="#">Strona główna</a>
+        <a  href="#">Kontakt</a>
+        <a  href="#">Produkty</a>
+        
+</nav>
+    <div class="basket">
+        <a  href="#">Koszyk</a>
+    </div>
+</div>
+</haeder>
+
+
